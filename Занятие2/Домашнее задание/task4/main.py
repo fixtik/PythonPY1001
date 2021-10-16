1ccdb72e0478eef569da891b51f8efbc9af6ef84
Alex <alx@mail.ru>
2021-10-16 16:51:17 +0300

    no comment

--- Занятие2/Домашнее задание/task4/main.py
+++ Занятие2/Домашнее задание/task4/main.py
@@ -1,11 +1,6 @@
 if __name__ == "__main__":
     list_= [12,5,4,98,32,1,58,32,90,8] #list(range(10))
-    max_ = 0
-    max_index = 0
-    for index, curr in enumerate(list_):
-        if max_ <= curr:
-            max_ = curr
-            max_index = index
+    max_index = list_.index(max(list_))
     list_[0], list_[max_index] = list_[max_index], list_[0]
 
     print(list_)
