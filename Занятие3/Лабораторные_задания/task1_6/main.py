02ac0a2e85ea460a35ed71e8547b1825dcefa601
Alex <alx@mail.ru>
2021-10-23 16:51:35 +0300

    Занятие 2 + ДЗ

--- Занятие3/Лабораторные_задания/task1_6/main.py
+++ Занятие3/Лабораторные_задания/task1_6/main.py
@@ -4,6 +4,7 @@ def student(stipends=10000, rash=12000, persent=3):
     for i in range(1,n+1):
         total_need += rash
         rash += rash * (persent / 100)
+
     return total_need - stipends * n
 
 
