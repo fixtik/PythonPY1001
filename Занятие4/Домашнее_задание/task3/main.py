5b6ccb26b2ad4c80d11749336b7cc449e00fae23
Alex <alx@mail.ru>
2021-11-01 11:00:57 +0300

    Занятие 4 + ДЗ (дополнительное задание с map в task3 дз)

--- Занятие4/Домашнее_задание/task3/main.py
+++ Занятие4/Домашнее_задание/task3/main.py
@@ -6,14 +6,17 @@ if __name__ == "__main__":
 #``INPUT:{"Наука": [88, 89, 62, 95], "Язык": [77, 78, 84, 80]}```
 # OUTPUT: [{'Наука':88,'Язык':77},{'Наука':89,'Язык':78}, {'Наука': 62, 'Язык': 84}, {'Наука': 95, 'Язык': 80}]```
 
-    def create_dict(item):
-        print(item)
-        return None
+    def create_dict(key, item):
+        m = (map(lambda i: {key: i},  item))
+        return list(m)
+
+    def sep_list(dict1, dict2):
+        return dict1 | dict2
 
     def task(dict_):
-        n = map(create_dict, dict_.values())
+        n = list(map(create_dict, dict_, dict_.values()))
+        m = map(sep_list, n[0], n[1])
 
-
-        return list(n)
+        return list(m)
 
     print(task({"Наука": [88, 89, 62, 95], "Язык": [77, 78, 84, 80]}))
